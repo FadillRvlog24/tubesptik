3d6467d1d243f3cc00e889a59d174076a8be96a9
MuhammadCepy <mcepy123@gmail.com>
2023-12-13 21:10:35 +0700

    commit2

--- progresptik.cpp
+++ progresptik.cpp
@@ -73,7 +73,7 @@ void DatabaseDosen::lihatDataDosen() const {
         std::cout << "Email: " << dataDosen[i].email << "\n";
         std::cout << "Nomor Telepon: " << dataDosen[i].nomorTelepon << "\n";
         std::cout << "Mata Kuliah: " << dataDosen[i].mataKuliah << "\n";
-		 std::cout << "Hari Mengajar: " << dataDosen[i].hariMengajar << "\n";
+		std::cout << "Hari Mengajar: " << dataDosen[i].hariMengajar << "\n";
         std::cout << "Umur: " << dataDosen[i].umur << " tahun\n\n";
        
         
@@ -81,4 +81,81 @@ void DatabaseDosen::lihatDataDosen() const {
 }	else {
 		std::cout<<"Belum ada data dosen. \";
 		}	std::cout<<"----------------------------------------------\n";
-}        
+}   
+     
+// Implementasi fungsi untuk mengupdate data dosen berdasarkan nama
+void DatabaseDosen::updateDataDosen(const std::string& namaDosen, const Dosen& newData) {
+    int index = -1;
+    for (int i = 0; i < jumlahDosen; ++i) {
+        if (dataDosen[i].nama == namaDosen) {
+            index = i;
+            break;
+        }
+    }
+
+    if (index != -1) {
+        dataDosen[index] = newData;
+        std::cout << "Data dosen berhasil diupdate.\n";
+    } else {
+        std::cout << "Dosen dengan nama '" << namaDosen << "' tidak ditemukan. Apakah Anda ingin menambahkannya? (Y/N): ";
+        char tambahData;
+        std::cin >> tambahData;
+
+        if (std::toupper(tambahData) == 'Y') {
+            tambahDataDosen(newData);
+            std::cout << "Data dosen berhasil ditambahkan.\n";
+        } else {
+            std::cout << "Update dibatalkan.\n";
+        }
+    }
+}
+
+// Implementasi fungsi untuk menghapus data dosen berdasarkan nama
+void DatabaseDosen::hapusDataDosen(const std::string& namaDosen) {
+    int index = -1;
+    for (int i = 0; i < jumlahDosen; ++i) {
+        if (dataDosen[i].nama == namaDosen) {
+            index = i;
+            break;
+        }
+    }
+
+    if (index != -1) {
+        for (int i = index; i < jumlahDosen - 1; ++i) {
+            dataDosen[i] = dataDosen[i + 1];
+        }
+        --jumlahDosen;
+        std::cout << "Data dosen berhasil dihapus.\n";
+    } else {
+        std::cout << "Dosen dengan nama '" << namaDosen << "' tidak ditemukan. Hapus dibatalkan.\n";
+    }
+}
+
+// Implementasi fungsi untuk validasi input hari
+bool DatabaseDosen::isValidHari(const std::string& hari) const {
+    std::string hariUpper;
+    std::transform(hari.begin(), hari.end(), std::back_inserter(hariUpper), ::toupper);
+    
+    std::vector<std::string>daftarHari = {"SENIN", "SELASA", "RABU", "KAMIS", "JUMAT"};
+    	return std::find(daftarHari.begin(), daftarHari.end(), hariUpper) != daftarHari.end();
+}
+
+// Implementasi fungsi untuk validasi apakah string hanya berisi huruf
+bool DatabaseDosen::isAlpha(const std::string& str) const {
+    return std::all_of(str.begin(), str.end(), ::isalpha);
+}
+
+// Implementasi fungsi untuk validasi apakah string hanya berisi angka
+bool DatabaseDosen::isNumeric(const std::string& str) const {
+    return std::all_of(str.begin(), str.end(), ::isdigit);
+}
+
+// Implementasi fungsi untuk validasi apakah nama dosen sudah ada dalam database
+bool DatabaseDosen::isDosenExist(const std::string& namaDosen) const {
+    for (int i = 0; i < jumlahDosen; ++i) {
+        if (dataDosen[i].nama == namaDosen) {
+            return true; // Nama dosen ditemukan
+        }
+    }
+    return false; // Nama dosen tidak ditemukan
+}
